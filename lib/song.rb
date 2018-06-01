262ca08772751450332b975b9e44f671eeec4365
Natalia Castro <nat.alvz@gmail.com>
2018-06-01 20:23:11 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -9,5 +9,45 @@ class Song
   def save
     self.class.all << self
   end
-
+  
+  def self.create
+    song = self.new
+    song.save
+    song
+  end
+  
+  def self.new_by_name(song)
+    new_song = self.new
+    new_song.name = song
+    new_song
+  end
+  
+  def self.create_by_name(name)
+    song = self.new
+    song.name = name
+    self.all << song 
+    return song
+  end
+  
+  def self.find_by_name(name)
+    self.all.find do |song|
+      return song if song.name == name
+    end
+    return nil
+  end
+  
+  def self.find_or_create_by_name(name)
+    if self.find_by_name(name) == nil
+      self.create_by_name(name)
+    else
+      self.find_by_name(name)
+    end
+  end
+  
+  def self.alphabetical
+    @@all.sort_by {|a,b| 
+    a <=> b}
+    
+  end
+    
 end
